83014344879817e99c5433a96d1b56cc0a2af7d7
WILLIAMCHOR <estudantewilliamchor@gmail.com>
2024-09-13 10:03:25 -0300

    Melhorando o nav e o header

---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+window.addEventListener('scroll', function() {
+    this.document.getElementById('nav').style.width = '100%';
+});
+const nav = document.getElementById('nav');
+window.addEventListener('scrollend',function() {
+    
+    nav.addEventListener('mouseover', (event) => {
+        this.document.getElementById('nav').style.width = '100%';
+    }); 
+    this.document.getElementById('nav').style.width = '0%';
+}); */
